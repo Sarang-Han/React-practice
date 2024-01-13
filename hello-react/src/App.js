1fea67d4ee2a14420d77e0dd01bd1d86040c1eb4
Sarang <hangpfm0518@ewhain.net>
2024-01-13 16:54:53 +0900

    클릭 액션

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function Square({value}) {
+  function handleClick(){
+    console.log('clicked!');
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <button 
+  className="square"
+  onClick={handleClick}
+  >
+    {value}
+  </button>
   );
 }
 
-export default App;
+export default function Board(){
+  return(
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="1"/>
+        <Square value="1"/>
+      </div>
+      <div className="board-row">
+        <Square value="1"/>
+        <Square value="1"/>
+        <Square value="1"/>
+      </div>
+      <div className="board-row">
+        <Square value="1"/>
+        <Square value="1"/>
+        <Square value="1"/>
+      </div>
+    </>
+  );
+}
