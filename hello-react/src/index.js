3eace7f48bccc002f9c18f73cc04c9b0cc842202
Sarang <hangpfm0518@ewhain.net>
2024-01-26 05:21:18 +0900

    온도계산기 실습

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import SignUp from './chap11/SignUp';
+import Calculator from './chap12/Calculator';
 
 const rootNode = document.getElementById('root');
 
 ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
-    <SignUp />
+    <Calculator />
   </React.StrictMode>,
 );
 
